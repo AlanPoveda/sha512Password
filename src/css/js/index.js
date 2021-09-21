130edc9e0f25bc409a5528df3d98db4760478fa2
Alan Poveda <alsepoveda@gmail.com>
2021-09-20 22:35:12 -0300

    Deletando pastas de js

--- src/css/js/index.js
+++
@@ -1,6 +0,0 @@
-const inputUser = document.getElementById("inputValue")
-const response = document.getElementById("response")
-
-function encrypt(){
-    response.innerHTML = `<h1>${SHA512(inputUser.value)}</h1>`
-}
