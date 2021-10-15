23d1331ffb2baa738676eae2e24ac01e6995be08
Alan Poveda <alsepoveda@gmail.com>
2021-10-15 08:55:30 -0300

    Arrumando alguns detalhes, para não mostrar a senha, e padding top também para não ficar tão grudado no topo

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ const inputUser = document.getElementById("inputValue")
 const response = document.getElementById("response")
 
 function encrypt(){
-    response.innerHTML = `${SHA512(inputUser.value)}`
+    response.innerHTML = `Password created`
 }
 
 function copyText(){
